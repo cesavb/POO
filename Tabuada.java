8580f716cc49029639328c268c1954bd2fb14e97
Cesavb <cesavb@gmail.com>
2021-03-03 16:17:40 -0300

    Add files via upload

---
+++ Tabuada.java
@@ -0,0 +1,17 @@
+package Tabuada;
+
+public class Tabuada {
+
+	public static void main(String[] args) {
+		
+		int tb=10;
+		
+		for(int i=1; i<=10; i++) {
+					
+			System.out.println(tb + ("x") + i + ("=") + (i*tb));
+			
+		}
+
+	}
+
+}
